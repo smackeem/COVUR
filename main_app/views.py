d194843df6da19f16d941896099e995369f759dc
smackeem <smackeem@outlook.com>
2024-01-12 14:43:51 -0500

    Create a template to display the products in the catalog.

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,14 @@
 from django.shortcuts import render
 
+catalogs = [
+    {'name': 'Day Moisturizer', 'description': 'SPF 15', 'price': 34.99, 'quantity': 40, 'image': 'https://picsum.photos/200/300'},
+    {'name': 'Toner', 'description': 'Blemish remover', 'price': 29.99, 'quantity': 40, 'image': 'https://picsum.photos/200/300'},
+    {'name': 'Eye Cream', 'description': 'Anti-Age', 'price': 15.99, 'quantity': 40, 'image': 'https://picsum.photos/200/300'},
+]
+
 # Create your views here.
 def home(request):
     return render(request, 'home.html')
+
+def catalog(request):
+    return render(request, 'products/index.html', {'catalog': catalogs})
