22e0c085e569ed72f5693514c4fa7fb5e38eaf46
smackeem <smackeem@outlook.com>
2024-01-14 14:21:11 -0500

    Created Cart, CartItem, Order and orderItem models.

--- main_app/models.py
+++ main_app/models.py
@@ -16,20 +16,45 @@ class Product(models.Model):
     quantity = models.IntegerField(default=0)
     image = models.ImageField(upload_to='static/product_images/')
 
+    def increase_quantity(self):
+        self.quantity += 1
+
+    def decrease_quantity(self):
+        self.quantity -= 1
+
     def __str__(self):
         return self.name
 
+class CartItem(models.Model):
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
 
-# class Cart(models.Model):
-#     customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
-#     product = models.ForeignKey(Product, on_delete = models.CASCADE)
+    def increase_quantity(self):
+        self.quantity += 1
 
-# class Order(models.Model):
-#     customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
-#     product = models.ForeignKey(Product, on_delete = models.CASCADE)
-#     date = models.DateTimeField()
-#     total = models.FloatField()
-#     status = models.BooleanField(default=False)
+    def decrease_quantity(self):
+        self.quantity -= 1
+
+    def __str__(self):
+        return f"{self.quantity} {self.product.name}"
+
+class Cart(models.Model):
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    product = models.ForeignKey(CartItem, on_delete = models.CASCADE)
+    total = models.ForeignKey()
+
+class OrderItem(models.Model):
+    cart = models.ForeignKey(Cart, on_delete = models.CASCADE)
+
+
+class Order(models.Model):
+    customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
+    product_list = models.ForeignKey(OrderItem, on_delete = models.CASCADE)
+    date = models.DateTimeField()
+    total = models.FloatField()
+    status = models.BooleanField(default=False)
+
+
 
 # class Review(models.Model):
 #     customer = models.ForeignKey(Customer, on_delete = models.CASCADE)
