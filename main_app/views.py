9be244c5e9e993450f5759f4235bb77656e4282f
smackeem <smackeem@outlook.com>
2024-01-13 19:45:38 -0500

    Created view of login_view to render authentication form on login.html

--- main_app/views.py
+++ main_app/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render, redirect
 from django.contrib.auth import login, logout, authenticate
+from django.contrib.auth.forms import AuthenticationForm
 from .forms import SignUpForm
 from django.contrib import messages
 from .models import Product
@@ -36,3 +37,15 @@ def signup(request):
     else:
         form = SignUpForm()
     return render(request, 'signup.html', {'form': form})
+
+def login_view(request):
+    if request.method == 'POST':
+        form = AuthenticationForm(request, data=request.POST)
+        if form.is_valid():
+            login(request, form.get_user())
+            return redirect('catalog')
+        else:
+            print(form.errors)
+    else:
+        form = AuthenticationForm()
+    return render(request, 'login.html', {'form': form})
