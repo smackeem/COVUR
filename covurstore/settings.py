6ef0b0d22eb27b5796a4ceefd965b3df1fab3f76
smackeem <smackeem@outlook.com>
2024-01-22 09:43:40 -0500

    Media root to media

--- covurstore/settings.py
+++ covurstore/settings.py
@@ -126,8 +126,8 @@ STATICFILES_DIRS = [BASE_DIR/'static']
 
 LOGIN_URL ='/account/login/'
 
-MEDIA_URL = "/image/"
-MEDIA_ROOT = [BASE_DIR/'media']
+MEDIA_URL = "/media/"
+MEDIA_ROOT = BASE_DIR/"media"
 
 
 # Default primary key field type
